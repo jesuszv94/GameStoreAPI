46d5b22f1ffac13666a49913ef749e1590660fb5
Jesus Zuniga <jesus.zuniga@globant.com>
2023-06-23 18:56:00 -0600

    Added first module of the course

---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,83 @@
+using GameStore.Api.Entities;
+
+const string GetGameEndpointName = "GetGame";
+
+List<Game> games = new()
+{
+    new Game()
+    {
+        Id = 1,
+        Name = "Street Fighter II",
+        Genre = "Fighting",
+        Price = 10.99M,
+        ReleaseDate = new DateTime(1991, 2, 1),
+        ImageUrl = "https://placehold.co/100",
+    },
+    new Game()
+    {
+        Id = 2,
+        Name = "Final Fantasy XIV",
+        Genre = "RolePlaying",
+        Price = 59.99M,
+        ReleaseDate = new DateTime(2010, 9, 30),
+        ImageUrl = "https://placehold.co/100",
+    },
+    new Game()
+    {
+        Id = 3,
+        Name = "FIFA 23",
+        Genre = "Sports",
+        Price = 69.99M,
+        ReleaseDate = new DateTime(2022, 9, 27),
+        ImageUrl = "https://placehold.co/100",
+    }
+};
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/games", () => games);
+app.MapGet("/games/{id}", (int id) =>
+{
+    Game? game = games.Find(game => game.Id == id);
+    return game is null ? Results.NotFound() : Results.Ok(game);
+})
+.WithName(GetGameEndpointName);
+
+app.MapPost("/games", (Game game) =>
+{
+    game.Id = games.Max(game => game.Id) + 1;
+    games.Add(game);
+    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+});
+
+app.MapPut("/games/{id}", (int id, Game updatedGame) =>
+{
+    Game? existingGame = games.Find(game => game.Id == id);
+
+    if (existingGame is null)
+    {
+        return Results.NotFound();
+    }
+
+    existingGame.Name = updatedGame.Name;
+    existingGame.Genre = updatedGame.Genre;
+    existingGame.Price = updatedGame.Price;
+    existingGame.ReleaseDate = updatedGame.ReleaseDate;
+    existingGame.ImageUrl = updatedGame.ImageUrl;
+    return Results.NoContent();
+});
+
+app.MapDelete("/games/{id}", (int id) =>
+{
+    Game? game = games.Find(game => game.Id == id);
+    if (game is not null)
+    {
+        games.Remove(game);
+    }
+    return Results.NoContent();
+});
+
+
+app.Run();
