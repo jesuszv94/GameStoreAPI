5adb82fe14b789ce227db32340fabe5a371de052
JZunigaV <jesus.zuniga@easygoband.com>
2023-07-03 22:30:24 -0600

    added DTOS

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,7 +1,10 @@
 using GameStore.Api.Endpoints;
-using GameStore.Api.Entities;
+using GameStore.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<IGamesRepository, InMemGamesRepository>();
+
+
 var app = builder.Build();
 app.MapGamesEndpoints();
 
